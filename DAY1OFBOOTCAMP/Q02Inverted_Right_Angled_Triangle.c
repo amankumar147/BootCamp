692996ddc37d0f1fa8fe96e64493e9e606ba2174
amankumar147 <24150cn147@glbitm.ac.in>
2025-01-29 19:15:26 +0530

    Q 02Inverted Right Angled Triangle

---
+++ DAY1OFBOOTCAMP/Q02Inverted_Right_Angled_Triangle.c
@@ -0,0 +1,23 @@
+/*
+Q 02Inverted Right Angled Triangle
+
+*****
+****
+***
+**
+*
+
+*/
+
+#include<stdio.h>
+
+int main(){
+    int i,j;
+
+    for(i=1;i<=5;i++){
+        for(j=5;j>=i;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
